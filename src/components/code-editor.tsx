364ff0c7cb6e7b05455acf73b81e5e1c963c32a2
Daniel Pataki <ptamasdaniel92@gmail.com>
2022-03-26 22:14:48 +0100

    style format button

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,8 @@
+import './code-editor.css';
 import { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
-import prettier from 'prettier';
-import parser from 'prettier/parser-babel';
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -24,36 +25,41 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     const unformatted = editorRef.current.getModel().getValue();
 
     const formatted = prettier.format(unformatted, {
-      parser: 'babel',
+      parser: "babel",
       plugins: [parser],
       useTabs: false,
       semi: true,
-      singleQuote: true
-    });
+      singleQuote: true,
+    }).replace(/\n$/, '');
 
     editorRef.current.setValue(formatted);
   };
 
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
-    <MonacoEditor
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-      editorDidMount={onEditorDidMount}
-      value={initialValue}
-      theme="dark"
-      language="javascript"
-      height="500px"
-    />
+    <div className='editor-wrapper'>
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        theme="dark"
+        language="javascript"
+        height="500px"
+      />
     </div>
   );
 };
